c6f750629d35b5b3d52277d2950aacdae898a7fc
unknown <timgarwood@gmail.com>
2019-12-15 23:26:53 -0500

    forgot file

---
+++ Game1/Game1/Physics/CollisionCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Physics
+{
+    public struct CollisionCategory
+    {
+        public static ushort Player { get; private set; } = 0x0001;
+        public static ushort Alien { get; private set; } = 0x0002;
+        public static ushort PlayerProjectile { get; private set; } = 0x0004;
+        public static ushort AlienProjectile { get; private set; } = 0x0008;
+    }
+}
